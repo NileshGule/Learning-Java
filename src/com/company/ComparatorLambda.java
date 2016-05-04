cd4f2035ec852c5fccb758829e70c182197d82e4
Nilesh Gule <vn_nilesh@yahoo.com>
2016-05-04 22:58:18 +0800

    multiple ways of using lambdas, last one (v4) being everything inlined into the Sort method instead of using comparator instances

--- src/com/company/ComparatorLambda.java
+++ src/com/company/ComparatorLambda.java
@@ -10,6 +10,7 @@ import java.util.List;
 public class ComparatorLambda {
 
     public static void main(String[] args){
+        //V1 -> with anonymous class
 //        Comparator<String> stringComparator = new Comparator<String>() {
 //            @Override
 //            public int compare(String s1, String s2) {
@@ -17,11 +18,18 @@ public class ComparatorLambda {
 //            }
 //        };
 
-        Comparator<String> stringComparator = (String s1, String s2) -> Integer.compare(s1.length(), s2.length());
-        
+        //V2 -> with 1st version of Lambda
+        //Comparator<String> stringComparator = (String s1, String s2) -> Integer.compare(s1.length(), s2.length());
+
+        //V3 -> with automatic referencing of data types for parameters
+        Comparator<String> stringComparator = (s1, s2) -> Integer.compare(s1.length(), s2.length());
+
         List<String> list = Arrays.asList("***", "*", "****", "**");
 
-        list.sort(stringComparator);
+        //list.sort(stringComparator);
+
+        // V4 -> most advanced lambda version with inlined expression instead of comparator
+        list.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
 
         for(String s : list){
             System.out.println(s);
