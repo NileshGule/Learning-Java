74a6fc6dbd6a67a048643936802f734374bda942
Nilesh Gule <vn_nilesh@yahoo.com>
2016-05-04 22:52:42 +0800

    string comparator without anonymous class

--- src/com/company/ComparatorLambda.java
+++ src/com/company/ComparatorLambda.java
@@ -10,13 +10,15 @@ import java.util.List;
 public class ComparatorLambda {
 
     public static void main(String[] args){
-        Comparator<String> stringComparator = new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                return Integer.compare(s1.length(), s2.length());
-            }
-        };
+//        Comparator<String> stringComparator = new Comparator<String>() {
+//            @Override
+//            public int compare(String s1, String s2) {
+//                return Integer.compare(s1.length(), s2.length());
+//            }
+//        };
 
+        Comparator<String> stringComparator = (String s1, String s2) -> Integer.compare(s1.length(), s2.length());
+        
         List<String> list = Arrays.asList("***", "*", "****", "**");
 
         list.sort(stringComparator);
