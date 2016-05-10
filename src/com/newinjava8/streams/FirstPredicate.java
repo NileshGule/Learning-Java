73b4beefe5dbd26f58beedbd679405e751e9c77d
Nilesh Gule <vn_nilesh@yahoo.com>
2016-05-10 23:23:23 +0800

    implemented first predicate with basic predicates. chained predicates using default and and or methods of predicate interface

--- src/com/newinjava8/streams/FirstPredicate.java
+++ src/com/newinjava8/streams/FirstPredicate.java
@@ -1,7 +1,23 @@
 package com.newinjava8.streams;
 
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
 /**
  * Created by Nilesh on 5/10/2016.
  */
 public class FirstPredicate {
+
+    public static void main(String... args){
+        Stream<String> stream = Stream.of("one", "two", "three", "four");
+
+        Predicate<String> p1 = s -> s.length() > 3;
+
+        Predicate<String> p2 = Predicate.isEqual("two");
+        Predicate<String> p3 = Predicate.isEqual("three");
+
+        //stream.filter(p2.or(p3))
+        stream.filter(p1.or(p2).or(p3))
+                .forEach(s -> System.out.println(s));
+    }
 }
