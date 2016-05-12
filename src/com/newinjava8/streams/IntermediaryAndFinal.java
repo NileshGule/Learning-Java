2580b282673390eb018b85af827c4905862de4c9
Nilesh Gule <vn_nilesh@yahoo.com>
2016-05-12 23:00:31 +0800

    adding class IntermediaryAndFinal

---
+++ src/com/newinjava8/streams/IntermediaryAndFinal.java
@@ -0,0 +1,28 @@
+package com.newinjava8.streams;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+/**
+ * Created by Nilesh on 5/12/2016.
+ */
+public class IntermediaryAndFinal {
+    public static void main(String... args){
+        Stream<String> stream = Stream.of("one", "two", "three", "four", "five");
+
+        Predicate<String> p1 = Predicate.isEqual("two");
+        Predicate<String> p2 = Predicate.isEqual("three");
+
+        List<String> list = new ArrayList<>();
+
+        stream
+                .peek(System.out::println)
+                .filter(p1.or(p2))
+                .forEach(list::add);
+
+        System.out.println("Done!");
+        System.out.println("size = " + list.size());
+    }
+}
